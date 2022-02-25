398811ec86ec8a2da6733073aee81d284f29c0c2
Andrej Turk <andrej.turk91@gmail.com>
2022-02-25 10:54:00 +0100

    Delete Custom Resources

--- prometheus/uninstall_prometheus_stack.sh
+++ prometheus/uninstall_prometheus_stack.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 helm uninstall prometheus-stack
+kubectl delete crd alertmanagerconfigs.monitoring.coreos.com
+kubectl delete crd alertmanagers.monitoring.coreos.com
+kubectl delete crd podmonitors.monitoring.coreos.com
+kubectl delete crd probes.monitoring.coreos.com
+kubectl delete crd prometheuses.monitoring.coreos.com
+kubectl delete crd prometheusrules.monitoring.coreos.com
+kubectl delete crd servicemonitors.monitoring.coreos.com
+kubectl delete crd thanosrulers.monitoring.coreos.com
